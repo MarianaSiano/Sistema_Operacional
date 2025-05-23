151956637272beb4f8149cfb7fdb4d7a349d8b6a
MarianaSiano <marianasianop@gmail.com>
2025-05-23 20:09:31 -0300

    Recomecando o arquivo 'Processos.c' e '.gitignore' modificado

---
+++ Atividades/01_Processos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+#include <process.h>
+//#include <sys/wait.h>
+
+//Compilacao condicional
+#define SLEEP(sec) Sleep((sec) * 1000)
+
+int main()
+{
+    return 0;
+}

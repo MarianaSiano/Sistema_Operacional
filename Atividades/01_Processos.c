2ae496705b0071f0cdb4ebc32ff818293331bb44
MarianaSiano <marianasianop@gmail.com>
2025-05-23 20:40:42 -0300

    Atualizando o arquivo

--- Atividades/01_Processos.c
+++ Atividades/01_Processos.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <windows.h>
-#include <process.h>
-//#include <sys/wait.h>
-
-//Compilacao condicional
-#define SLEEP(sec) Sleep((sec) * 1000)
+// #include <windows.h>
+// #include <process.h>
+#include <sys/wait.h>
 
 int main()
 {
+    for(int i = 0; i < 3; i++) {
+        pid_t pid = fork();
+        if(pid < 0) {
+            perror("Erro ao criar processo filho");
+            exit(1);
+        }
+
+        else if(pid == 0) {
+            //Codigo do processo filho
+            printf("Processo filho (PID: %d), PID do pai: %d\n", getpid(), getppid());
+            exit(0); //Filho finaliza aqui
+        }
+    }
+
+    //Codigo do processo pai
+    for(int i = 0; i < 3; i++)
+        wait(NULL); //Espera os 3 filhos finalizarem
+
+    printf("Processo pai finalizado\n");
     return 0;
 }

b0b45d796d0f10ffd4af5edff06f1596ee1a7d1b
MarianaSiano <marianasianop@gmail.com>
2025-05-23 14:47:22 -0300

    Comecando a ativiade

--- Atividades/Capitulo02_Parte01.c
+++ Atividades/Capitulo02_Parte01.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() 
+int main()
 {
+    int opcao;
+
+    do {
+        printf("\n================= MENU =================\n");
+        printf("1.");
+        printf("0. Sair\n");
+        printf("Escolha uma opcao => ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1: {
+                break;
+            }
+
+            case 0: {
+                printf("Encerrando o programa...\n");
+                break;
+            }
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+    } while(opcao != 0);
     return 0;
 }

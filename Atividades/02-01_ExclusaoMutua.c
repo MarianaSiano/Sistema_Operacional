af2c527c6ae5f2c26d1d2429fe7d64fbff5492ae
MarianaSiano <marianasianop@gmail.com>
2025-05-24 09:51:40 -0300

    Novo arquivo para Exclusao mutua

---
+++ Atividades/02-01_ExclusaoMutua.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+#include <process.h>
+#include <pthread.h>
+//#include <sys/wait.h>
+
+//Atividade 2 - Exclusão Mútua
+//2.1 - Incremento com e sem mutex
+
+int main()
+{
+    return 0;
+}

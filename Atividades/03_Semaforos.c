722c13b6b6cc315712e43926d60287cc94fdb2ba
MarianaSiano <marianasianop@gmail.com>
2025-05-24 10:27:03 -0300

    Criando um arquivo da terceira atividade

---
+++ Atividades/03_Semaforos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+#include <process.h>
+#include <pthread.h>
+#include <semaphore.h>
+//#include <sys/wait.h>
+
+//Atividade 3 - Semáforos (Produtor-Consumidor)
+int contador = 0;
+pthread_mutex_t lock;
+
+int main()
+{
+    return 0;
+}

9cd0b8e4be00a457bea0e39d71b0781178ad0536
MarianaSiano <marianasianop@gmail.com>
2025-05-24 10:48:10 -0300

    funcoes feitas

--- Atividades/03_Semaforos.c
+++ Atividades/03_Semaforos.c
@@ -8,8 +8,39 @@
 //#include <sys/wait.h>
 
 //Atividade 3 - Semáforos (Produtor-Consumidor)
-int contador = 0;
-pthread_mutex_t lock;
+#define TAM 5
+int buffer[TAM];
+int in = 0, out = 0;
+
+sem_t empty, full;
+pthread_mutex_t mutex;
+
+void *produtor(void *arg)
+{
+    for(int i = 1; i <= 10; i++) {
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        buffer[in] = i;
+        in = (in + 1) % TAM;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+    return NULL;
+}
+
+void *consumidor(void *arg)
+{
+    for(int i = 1; i <= 10; i++) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int valor = buffer[out];
+        out = (out + 1) % TAM;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+        printf("Consumidor: %d\n", valor);
+    }
+    return NULL;
+}
 
 int main()
 {

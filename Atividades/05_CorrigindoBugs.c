9496f8049408c467b876835305ee72b892b4857d
MarianaSiano <marianasianop@gmail.com>
2025-05-24 13:13:57 -0300

    Atividade 5.2 concluida

--- Atividades/05_CorrigindoBugs.c
+++ Atividades/05_CorrigindoBugs.c
@@ -7,23 +7,47 @@
 #include <semaphore.h>
 //#include <sys/wait.h>
 
-//Atividade 5 - Correção de Bugs
+//Atividade 5.2
+int counter = 0;
+pthread_mutex_t lock;
 
+//Original
+void *increment1(void *arg)
+{
+    for(int i = 0; i < 1000; i++)
+        counter++;
+    return NULL;
+}
+
+//Corrigido
+void *increment(void *arg)
+{
+    for(int i = 0; i < 1000; i++) {
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+//Atividade 5 - Correção de Bugs
 int main()
 {
-    //Atividade 5.1 - Por que este código cria Processos Zumbis?
     int opcao;
     do {
         printf("\n======================= MENU  =======================\n");
         printf("\n======================= ATIVIDADE 5.1 =======================\n");
         printf("1. Codigo Original\n");
         printf("2. Codigo Corrigido\n");
+        printf("\n======================= ATIVIDADE 5.2 =======================\n");
+        printf("3. Codigo Original\n");
+        printf("4. Corrigir Erros de Sincronizacao");
         printf("0. Sair\n");
         printf("Escolha uma opcao => ");
 
         switch(opcao) {
             case 1: {
-                //Codigo original
+                //Codigo original da atividade 5.1 - Por que este código cria Processos Zumbis
                 for(int i = 0; i < 3; i++) {
                     if(fork() == 0) {
                         print("Filho %d criado\n", getpid());
@@ -51,6 +75,31 @@ int main()
                     wait(NULL);
 
                 printf("Pai Finalizado\n");
+                break;
+            }
+
+            case 3: {
+                pthread_t t1, t2;
+                pthread_create(&t1, NULL, increment1, NULL);
+                pthread_create(&t2, NULL, increment1, NULL);
+                printf("Couter => %d\n", counter);
+                break;
+            }
+
+            case 4: {
+                pthread_t t1, t2;
+
+                pthread_mutex_initi(&lock, NULL);
+
+                pthread_create(&t1, NULL, increment, NULL);
+                pthread_create(&t2, NULL, increment, NULL);
+
+                pthread_join(t1, NULL);
+                pthread_join(t2, NULL);
+
+                printf("Counter => %d\n", counter);
+
+                pthread_mutex_destroy(&lock);
                 break;
             }
 

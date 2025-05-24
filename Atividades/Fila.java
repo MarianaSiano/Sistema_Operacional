9c6427247e7870a085e46089ebfd282755f890ce
MarianaSiano <marianasianop@gmail.com>
2025-05-24 16:14:20 -0300

    Atualizando o passo a passo do Readme e atualizando o 'Fila.java'

--- Atividades/Fila.java
+++ Atividades/Fila.java
@@ -5,16 +5,59 @@ import java.util.Queue;
 
 //Atividade 5.4
 public class Fila {
-    private Queue<Integer> fila = new LinkedList<>();
+    private final Queue<Integer> fila = new LinkedList<>();
 
     public synchronized void adicionar(int item) {
         fila.add(item);
-        notify(); //Evita starvation
+        notifyAll(); //Evita starvation para múltiplos consumidores
     }
 
     public synchronized int remover() throws InterruptedException {
-        while(fila.isEmpty())
+        while(fila.isEmpty()) {
             wait();
+        }
         return fila.poll();
+    }
+
+    //Método main para teste com produtor e consumidor
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+
+        //Thread produtora
+        Thread produtor = new Thread(() -> {
+            for(int i = 1; i <= 5; i++) {
+                fila.adicionar(i);
+                System.out.println("Adicionado: " + i);
+                try {
+                    Thread.sleep(1000); //Simula tempo de produção
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //Thread consumidora
+        Thread consumidor = new Thread(() -> {
+            for(int i = 1; i <= 5; i++) {
+                try {
+                    int valor = fila.remover();
+                    System.out.println("Removido: " + valor);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        produtor.start();
+        consumidor.start();
+
+        try {
+            produtor.join();
+            consumidor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Fim da execução.");
     }
 }
--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 * Repositório para as atividades da disciplina DCC062 - Sistemas Operacionais <br>
 
-# Observação
+## Observação
 
 * As atividades da Linguagem C funcionam APENAS no Sistema Operacional Linux <br>
 
 * Linux utilizado para testes foi o Ubuntu 20.04 <br>
 
-# Compilando os Programas em Linguagem C
+* As atividades em Linguagem Java foi testada no Windows 11. 
+
+## Compilando os Programas em Linguagem C
 
 * Para rodar os programas que são da linguagem C, basta ir para a pasta de Atividades com o seguinte comando: <br>
 `cd Atividades`  <br>
@@ -24,3 +26,15 @@
 * Com isso, o programa irá rodar. <br>
 
 * Mas caso vai direto executar sem antes compilar o programa, o mesmo não irá funcionar.
+
+## Compilando os Programas em Linguagem Java no Windows
+
+* Para rodar as atividades da Linguagem Java, fique no diretório raiz (não entrando no diretório `Atividades`) <br>
+
+* No seu computador, precisa está instalado o JDE e o JDK e esse último, o `bin` tem que está obrigatoriamente no `PATH` em `Ambientes de Trabalho` <br>
+
+* Após as instalações, faça o seguinte comando: <br>
+`javac Atividades/nome_do_arquivo.java` <br>
+
+* Com isso, criará um executavel e execute: <br>
+`java Atividades/nome_do_arquivo`

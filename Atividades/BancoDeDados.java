78d2abd68569d8e4dffd5c6815aa6b751565df6f
MarianaSiano <marianasianop@gmail.com>
2025-05-24 16:24:58 -0300

    Atualizando o arquivo 'BancoDeDados.java' e 'BlockingQueue.java'

--- Atividades/BancoDeDados.java
+++ Atividades/BancoDeDados.java
@@ -1,30 +1,69 @@
 package Atividades;
+//Atividade 5.7
 
 public class BancoDeDados {
     private int leitores = 0;
     private boolean escrevendo = false;
 
-    public synchronized void IniciarLeitura() throws InterruptedException {
-        while(escrevendo)
+    public synchronized void iniciarLeitura() throws InterruptedException {
+        while(escrevendo) {
             wait();
-
+        }
         leitores++;
     }
 
     public synchronized void terminarLeitura() {
         leitores--;
-        if(leitores == 0)
+        if(leitores == 0) {
             notifyAll();
+        }
     }
 
     public synchronized void iniciarEscrita() throws InterruptedException {
-        while(escrevendo || leitores > 0)
+        while(escrevendo || leitores > 0) {
             wait();
+        }
         escrevendo = true;
     }
 
     public synchronized void terminarEscrita() {
         escrevendo = false;
         notifyAll();
+    }
+
+    public static void main(String[] args) {
+        BancoDeDados banco = new BancoDeDados();
+
+        //Criando leitores
+        for(int i = 1; i <= 3; i++) {
+            int id = i;
+
+            new Thread(() -> {
+                try {
+                    banco.iniciarLeitura();
+                    System.out.println("Leitor " + id + " lendo...");
+                    Thread.sleep(1000); //Simula leitura
+                    System.out.println("Leitor " + id + " terminou de ler.");
+                    banco.terminarLeitura();
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
+        //Escritor
+        new Thread(() -> {
+            try {
+                Thread.sleep(500); //Espera leitores começarem
+                banco.iniciarEscrita();
+                System.out.println("Escritor escrevendo...");
+                Thread.sleep(2000); //Simula escrita
+                System.out.println("Escritor terminou de escrever.");
+                banco.terminarEscrita();
+            } catch 
+            (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
--- Atividades/BlockingQueue.java
+++ Atividades/BlockingQueue.java
@@ -1,7 +1,8 @@
 package Atividades;
 
-//4 - Monitores
-import java.util.*;
+//Atividade 4 - Monitores
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BlockingQueue<T> {
     private final Queue<T> queue = new LinkedList<>();
@@ -12,19 +13,63 @@ public class BlockingQueue<T> {
     }
 
     public synchronized void put(T item) throws InterruptedException {
-        while(queue.size() == maxSize)
+        while(queue.size() == maxSize) {
             wait();
+        }
 
         queue.add(item);
-        notify();
+        notifyAll(); //Acorda consumidores
     }
 
     public synchronized T take() throws InterruptedException {
-        while(queue.isEmpty())
+        while(queue.isEmpty()) {
             wait();
+        }
 
         T item = queue.poll();
-        notify();
+        notifyAll(); //Acorda produtores
         return item;
+    }
+
+    public static void main(String[] args) {
+        BlockingQueue<Integer> fila = new BlockingQueue<>(3);
+
+        //Produtor
+        Thread produtor = new Thread(() -> {
+            try {
+                for(int i = 1; i <= 5; i++) {
+                    fila.put(i);
+                    System.out.println("Produtor colocou: " + i);
+                    Thread.sleep(1000);
+                }
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        //Consumidor
+        Thread consumidor = new Thread(() -> {
+            try {
+                for(int i = 1; i <= 5; i++) {
+                    int valor = fila.take();
+                    System.out.println("Consumidor pegou: " + valor);
+                    Thread.sleep(1500);
+                }
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        produtor.start();
+        consumidor.start();
+
+        try {
+            produtor.join();
+            consumidor.join();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Fim da execução.");
     }
 }

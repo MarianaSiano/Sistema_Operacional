518120ee4245873604450bc88850fae7574f543c
MarianaSiano <marianasianop@gmail.com>
2025-05-24 09:57:54 -0300

    funcao concluida

--- Atividades/02-01_ExclusaoMutua.c
+++ Atividades/02-01_ExclusaoMutua.c
@@ -8,6 +8,18 @@
 
 //Atividade 2 - Exclusão Mútua
 //2.1 - Incremento com e sem mutex
+int contador = 0;
+pthread_mutex_t lock;
+
+void *incrementar(void *arg)
+{
+    for(int i = 0; i < 1000; i++) {
+        pthread_mutex_lock(&lock);
+        contador++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
 
 int main()
 {

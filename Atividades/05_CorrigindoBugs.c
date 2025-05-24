4f36dfb4340d9681638132d3802b823e34a4e03c
MarianaSiano <marianasianop@gmail.com>
2025-05-24 11:44:30 -0300

    Renomeando arquivo

---
+++ Atividades/05_CorrigindoBugs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+#include <process.h>
+#include <pthread.h>
+#include <semaphore.h>
+//#include <sys/wait.h>
+
+//Atividade 5 - Correção de Bugs
+
+int main()
+{
+    //Atividade 5.1 - Por que este código cria Processos Zumbis?
+    int opcao;
+    do {
+        printf("\n======================= MENU  =======================\n");
+        printf("\n======================= ATIVIDADE 5.1 =======================\n");
+        printf("1. Codigo Original\n");
+        printf("2. Codigo Corrigido\n");
+
+        switch(opcao) {
+            case 1: {
+                break;
+            }
+        }
+    } while(opcao != 0);
+    return 0;
+}
